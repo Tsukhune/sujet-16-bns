f8e78b1b751aff6b39b87a1d587556197e0b723c
Tsukhune <71658492+Tsukhune@users.noreply.github.com>
2021-02-27 14:51:31 +0100

    Update exercice2.py

--- exercices/exercice2.py
+++ exercices/exercice2.py
@@ -1,7 +1,7 @@
 def dec_to_bin(a):
-    bin_a = ...
+    bin_a = ""
     a = a//2
-    while a ... :
-        bin_a = ... + bin_a
-        a = ...
+    while a >0 :
+        bin_a = str(a/2) + bin_a
+        a = a//2
     return bin_a
